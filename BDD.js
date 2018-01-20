12140a19540c71cea79fcf03bd03a9c070bf20a1
Spokeek <spokeek@gmail.com>
2018-01-20 15:42:37 +0100

    first functions for BDD

---
+++ BDD.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const sqlite3 = require('sqlite3').verbose()
+
+const constants = require(path.resolve('./BDDConstants'))
+
+class Database{
+
+	init(){
+		return new Promise((res, rej) => {
+			this.getDb().run(constants.SQL_CREATE_TABLE, (err) => {
+				if(err){
+					rej(err)
+				}
+				else{
+					res()
+				}
+			})
+		})
+	}
+
+	getDb(){
+		if(!this.db){
+			this.db = new sqlite3.Database(path.resolve('./database.db'))
+		}
+		return this.db
+	}
+
+	addToken(newToken){
+		return new Promise((res, rej) => {
+			this.getDb().run(constants.SQL_ADD_TOKEN, {[constants.SQL_ADD_TOKEN_PARAM_VALUE]: newToken}, (err) => {
+				if(err){
+					rej(err)
+				}
+				else{
+					res()
+				}
+			})
+		})
+	}
+
+	getToken(token){
+		return new Promise((res, rej) => {
+			this.getDb().get(constants.SQL_GET_TOKEN, {[constants.SQL_GET_TOKEN_PARAM_VALUE]: token}, (err, row) => {
+				if(err){
+					rej(err)
+				}
+				else
+				res(row)
+			})
+		})
+	}
+
+	close(){
+		if(this.db){
+			return new Promise((res, rej) => {
+				this.db.close((err) => {
+					if(err){
+						rej(err)
+					}
+					else{
+						res()
+					}
+				})
+			})
+		}
+		else{
+			return Promise.resolve()
+		}
+	}
+}
+
+module.exports = Database
